5b881b55cddd3be3f4ef86c79e5cfabd1b27faf8
AustinS-Autie <71094074+AustinS-Autie@users.noreply.github.com>
2020-09-10 20:52:27 -0500

    First
    made project

---
+++ Code/JumpTest/Assets/Scripts/GroundInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundInfo : MonoBehaviour
+{
+    int colDist = 5;
+    int jump = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray r = new Ray(transform.position, Vector3.down);
+
+        Debug.DrawLine(r.origin, r.origin + (Vector3.down * colDist));
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(r, out hit, colDist))
+        {
+            Debug.Log(hit.transform.name);
+
+            if (hit.transform.GetComponent<GroundInfo>() != null)
+            {
+                jump = 0;
+            }
+        }
+    }
+}
---
+++ Code/JumpTest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5;
+    [SerializeField]
+    float jumpSpeed = 10;
+    [SerializeField]
+    int jumpTimes = 1;      
+
+    int jump = 0;
+
+    float colDist = 0.75f;
+
+
+    Rigidbody rBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray r = new Ray(transform.position, Vector3.down);
+
+        Debug.DrawLine(r.origin, r.origin + (Vector3.down * colDist) );
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(r, out hit, colDist) )
+        {
+            //Debug.Log(hit.transform.name);
+
+            if(hit.transform.GetComponent<GroundInfo>() != null)
+            {
+                //Debug.Log(hit.transform.GetComponent<GroundInfo>());
+                jump = 0;
+                
+                //Debug.Log("Jump is " + jump);     //jump display test
+            }
+        }
+        
+        
+        
+        
+        
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        rBody.velocity = new Vector3(hAxis * moveSpeed, rBody.velocity.y, vAxis * moveSpeed);
+
+        if(Input.GetButtonDown("Jump") && jump<jumpTimes)
+        {
+            Jump();
+            jump += 1;
+        }
+    }
+    
+    void Jump()
+    {
+        rBody.velocity = new Vector3(rBody.velocity.x, jumpSpeed, rBody.velocity.z);
+    }
+}
